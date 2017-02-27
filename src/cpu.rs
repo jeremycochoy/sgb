bbd6d5bb1b25381191f6d1e77c936e655df268a3
Jeremy Cochoy <jeremy.cochoy@gmail.com>
2017-02-27 14:28:54 +0100

    Add xor and a bit of or

--- src/cpu.rs
+++ src/cpu.rs
@@ -25,6 +25,14 @@ pub enum Register {
     F = 7,
 }
 
+/// List of flags
+pub enum Flag {
+    Z = 7,
+    N = 6,
+    H = 5,
+    C = 4,
+}
+
 /// Macro for easy access to registers
 ///
 /// Syntax : `reg![vm; register_name]`
@@ -59,12 +67,29 @@ macro_rules! hl {
     );
 }
 
+/// Macro for setting a u16 value into the register h:l
+/// (the juxtaposition of the two registers)
 macro_rules! set_hl {
     ($vm:expr, $value:expr) => {{
         let (h, l) = w_uncombine($value as u16);
         reg![$vm ; Register::H] = h;
         reg![$vm ; Register::L] = l;
     }}
+}
+
+/// Reset the flags of the Vm (set all flags to 0)
+pub fn reset_flags(vm: &mut Vm) {
+    reg![vm ; Register::F] = 0
+}
+
+/// Set the specified flag to the value given
+pub fn set_flag(vm : &mut Vm, flag : Flag, value : bool) {
+    if value {
+        reg![vm ; Register::F] |= 1 << flag as usize
+    }
+    else {
+        reg![vm ; Register::F] &= !(1 << flag as usize)
+    }
 }
 
 #[derive(PartialEq, Eq, Default, Debug)]
@@ -214,30 +239,84 @@ pub fn i_ldhld8(vm : &mut Vm) -> Clock {
     Clock { m:2, t:8 }
 }
 
-/// LD (a16) <- a where a16 means the next Word16 as an address.
+/// LD (a16) <- a where a16 means the next Word16 as an address
 pub fn i_lda16a(vm : &mut Vm) -> Clock {
     let a16 = read_program_word(vm);
     mmu::wb(a16, reg![vm ; Register::A], &mut vm.mmu);
     Clock { m:3, t:12 }
 }
 
-/// LD a <- (a16) where a16 means the next Word16 as an address.
+/// LD a <- (a16) where a16 means the next Word16 as an address
 pub fn i_ldaa16(vm : &mut Vm) -> Clock {
     let a16 = read_program_word(vm);
     reg![vm ; Register::A] = mmu::rb(a16, &vm.mmu);
     Clock { m:3, t:12 }
 }
 
-/// LD (a8) <- a where a8 means the next Word8 + 0xFF00 as an address.
+/// LD (a8) <- a where a8 means the next Word8 + 0xFF00 as an address
 pub fn i_lda8a(vm : &mut Vm) -> Clock {
     let a8 = read_program_byte(vm);
     mmu::wb(a8 as u16 + 0xFF00, reg![vm ; Register::A], &mut vm.mmu);
     Clock { m:3, t:12 }
 }
 
-/// LD a <- (a8) where a8 means the next Word8 + 0xFF00 as an address.
+/// LD a <- (a8) where a8 means the next Word8 + 0xFF00 as an address
 pub fn i_ldaa8(vm : &mut Vm) -> Clock {
     let a8 = read_program_byte(vm);
     reg![vm ; Register::A] = mmu::rb(a8 as u16 + 0xFF00, &vm.mmu);
     Clock { m:3, t:12 }
+}
+
+/// Implement xoring the register A with the value src_val
+pub fn i_xor_imp(src_val : u8, vm : &mut Vm) {
+    reg![vm ; Register::A] ^= src_val;
+    let result = reg![vm ; Register::A];
+    set_flag(vm, Flag::Z, result == 0);
+}
+
+/// XOR the register A with a register src into A
+/// Syntax : `XOR src:Register`
+pub fn i_xorrr(vm : &mut Vm, src : Register) -> Clock {
+    reset_flags(vm);
+    i_xor_imp(reg![vm ; src], vm);
+    Clock { m:1, t:8 }
+}
+
+/// XOR the register A with (HL) into A
+/// Syntax : `XORHL`
+pub fn i_xorhl(vm : &mut Vm) -> Clock {
+    reset_flags(vm);
+    i_xor_imp(mmu::rb(hl![vm], &vm.mmu), vm);
+    Clock { m:1, t:8 }
+}
+
+/// XOR the register A with immediate word8 into A
+/// Syntax : `XORd8`
+pub fn i_xord8(vm : &mut Vm) -> Clock {
+    reset_flags(vm);
+    let d8 = read_program_byte(vm);
+    i_xor_imp(d8, vm);
+    Clock { m:1, t:8 }
+}
+
+/// Implementation OR of a value with the register A, stored into A
+pub fn i_or_imp(src_val : u8, vm : &mut Vm) {
+    reset_flags(vm);
+    reg![vm ; Register::A] |= src_val;
+    let result = reg![vm ; Register::A];
+    set_flag(vm, Flag::Z, result == 0);
+}
+
+/// Bitwise OR the register A with a register src into A
+/// Syntax : `OR src`
+pub fn i_orr(vm : &mut Vm, src : Register) -> Clock {
+    i_or_imp(reg![vm ; src], vm);
+    Clock { m:1, t:4 }
+}
+
+/// Bitwise OR the register A with (HL) into A
+/// Syntax : `OR src`
+pub fn i_orhl(vm : &mut Vm) -> Clock {
+    i_or_imp(mmu::rb(hl![vm], &vm.mmu), vm);
+    Clock { m:1, t:8 }
 }
